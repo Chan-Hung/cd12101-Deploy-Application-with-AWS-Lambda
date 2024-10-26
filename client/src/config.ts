34245c11af54c441e47ea6083761f97df1e8d688
Hung Doan Chan <59687483+Chan-Hung@users.noreply.github.com>
2024-10-27 00:12:49 +0700

    Submit finale project Udacity Cloud Developer

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+/**
+ * Rubric requirement
+ * Once you have finished developing your application, please set apiId and Auth0 parameters in the config.ts file in the client folder
+ */
+export const AUTH0_DOMAIN='dev-68o1teqfu20pfskn.us.auth0.com'
+export const AUTH0_CLIENT_ID='Q0jymitQWzIGXN5CgeNrYnrSfryNDh71'
+export const AUTH0_AUDIENCE_ENDPOINT='https://dev-68o1teqfu20pfskn.us.auth0.com/api/v2/'
+export const API_ENDPOINT='https://a4pmf646ig.execute-api.us-east-1.amazonaws.com/dev'
